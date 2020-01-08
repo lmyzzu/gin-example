a3e0840212fe20270c0c7a63cc3410b7db7d3157
zsl <zhangshiliang@360.cn>
2020-01-08 11:34:39 +0800

    环境

--- demo12/main.go
+++ demo12/main.go
@@ -12,14 +12,18 @@ var env = pflag.StringP("env", "e", "", "set environment")
 
 func main() {
 	pflag.Parse()
-	if *env == "" {
-		log.Fatal("environment must be set!")
-	}
+	var env = *env
+
 	cfg, err := ini.Load("conf/app.ini")
 	if err != nil {
 		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
 	}
-	section := cfg.Section(*env)
+	if env == "" {
+		a := cfg.Section("env")
+		b, _ := a.GetKey("value")
+		env = b.String()
+	}
+	section := cfg.Section(env)
 	addr, err := section.GetKey("addr")
 	runMode, err := section.GetKey("runMode") //dev、test环境可以从父分区获取
 	r := gin.Default()
